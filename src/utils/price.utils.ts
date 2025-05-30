a5ba9610c99e130c7623e27a603e78ca650ff9fb
mainnet-pat <mainnet_pat@protonmail.com>
2025-05-30 13:08:21 +0000

    Add multicall2 for batch price rate requests

--- src/utils/price.utils.ts
+++ src/utils/price.utils.ts
@@ -1,12 +1,17 @@
 import { ethers } from "ethers";
 import { aerodromeContracts, rpcUrls } from "./config.utils";
-import { formatValue, fromUnits } from "./math.utils";
-import { FormattedPool } from "./sugar.utils";
 import offchainOracleAbi from "../abi/offchainOracle.json";
+import multicallAbi from "../abi/multicall.json";
+import { getToken } from "./token.utils";
 
 export const StableCoin: Record<number, string> = {
-  56: "0xa44319d6232afeaa21a38b040ca095110ad76d38", // tUSDT on BSC
+  56: "0xa44319D6232afEAa21A38b040Ca095110ad76d38", // tUSDT on BSC
   8453: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // USDC on BASE
+}
+
+export const multicallAddress: Record<number, string> = {
+  56: "0xed386Fe855C1EFf2f843B910923Dd8846E45C5A4", // Uniswap V3 Multicall on BSC
+  8453: "0xfee958fa595b4478cea7560c91400a98b83d6c91", // Uniswap V3 Multicall on BASE
 }
 
 export const getUsdPrice = async (chainId: number, token: string, amount: number): Promise<number> => {
@@ -26,12 +31,70 @@ export const getUsdRate = async (chainId: number, token: string): Promise<number
           new ethers.JsonRpcProvider(rpcUrls[chainId])
       );
 
-      console.log(token, StableCoin[chainId], chainId);
       const rate = await instance.getRate(token, StableCoin[chainId], true);
-      const formattedRate = Number(fromUnits(rate, 18));
-      return formattedRate;
+
+      const decimals = getToken(token)?.decimals || 18; // Default to 18 decimals if not found
+      const numerator = 10 ** decimals;
+      const denominator = 1e18;
+
+      return Number(rate) * numerator / denominator / 1e18;
   } catch (error) {
       console.log(error, chainId)
       return 0;
+  }
+};
+
+
+export const getUsdRates = async (chainId: number, tokens: string[]): Promise<Record<string, number>> => {
+  const result: Record<string, number> = {};
+  try {
+      if (tokens.length === 0) {
+          return result; // Return empty object if no tokens are provided
+      }
+
+      result[StableCoin[chainId]] = 1; // Set stablecoin rate to 1
+
+      const filteredTokens = tokens.filter(token => token.toLowerCase() !== StableCoin[chainId].toLowerCase());
+
+      const provider = new ethers.JsonRpcProvider(rpcUrls[chainId]);
+      const multicallContract = new ethers.Contract(multicallAddress[chainId], multicallAbi, provider);
+
+      const oracleContract = new ethers.Contract(
+          aerodromeContracts[chainId].offchainOracle as string,
+          offchainOracleAbi,
+          provider,
+      );
+
+      const calls = filteredTokens.map((token) => ({
+        target: oracleContract.target,
+        callData: oracleContract.interface.encodeFunctionData('getRate', [
+            token,
+            StableCoin[chainId],
+            true,
+        ]),
+      }));
+
+      // console.log(multicallContract)
+      const results = await multicallContract.tryAggregate.staticCall(false, calls);
+      console.log(results, 123);
+      for (let i = 0; i < results.length; i++) {
+          if (!results[i].success) {
+              result[filteredTokens[i]] = 0; // If the call failed, set rate to 0
+              continue;
+          }
+
+          const decodedRate = ethers.AbiCoder.defaultAbiCoder().decode(['uint256'], results[i].returnData).toString();
+
+          const decimals = getToken(filteredTokens[i])?.decimals || 18; // Default to 18 decimals if not found
+          const numerator = 10 ** decimals;
+          const denominator = 1e18;
+
+          result[filteredTokens[i]] = Number(decodedRate) * numerator / denominator / 1e18;
+      }
+
+      return result;
+  } catch (error) {
+      console.log(error, chainId)
+      return {};
   }
 };
